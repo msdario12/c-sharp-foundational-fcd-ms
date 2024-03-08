0200af9c37edfc8ce5cb3d913439f8334b9dc6d1
msdario12 <mansilladario94@gmail.com>
2024-03-08 08:17:32 -0300

    finish task

--- Program.cs
+++ Program.cs
@@ -1,57 +1,61 @@
-﻿
-Random random = new Random();
-
-Console.WriteLine("Would you like to play? (Y/N)");
-if (ShouldPlay())
+﻿string[] pettingZoo =
 {
-  PlayGame();
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+PlanSchoolVisit("Shool A");
+PlanSchoolVisit("Shool B", 3);
+PlanSchoolVisit("Shool C", 2);
+
+RandomizeAnimals();
+
+void PlanSchoolVisit(string schoolName, int groups = 6)
+{
+  RandomizeAnimals();
+  string[,] group = AssingGroup();
+  Console.WriteLine(schoolName);
+  PrintGroup(group);
 }
 
-bool ShouldPlay()
+void RandomizeAnimals()
 {
-  string userInput = "";
-  while (userInput != "exit")
+  Random random = new Random();
+  for (int i = 0; i < pettingZoo.Length; i++)
   {
-    string? readValue = Console.ReadLine();
-    if (readValue != null)
+    int r = random.Next(i, pettingZoo.Length);
+
+    string temp = pettingZoo[i];
+    pettingZoo[i] = pettingZoo[r];
+    pettingZoo[r] = temp;
+
+  }
+}
+
+string[,] AssingGroup(int groups = 6)
+{
+  string[,] result = new string[groups, pettingZoo.Length / groups];
+  int start = 0;
+  for (int i = 0; i < groups; i++)
+  {
+    for (int j = 0; j < result.GetLength(1); j++)
     {
-      userInput = readValue;
-      userInput.ToLower().Trim();
-      return userInput == "y" || userInput == "yes";
+      result[i, j] = pettingZoo[start++];
     }
   }
-  return false;
+  return result;
+
 }
-
-int GetRollValue()
+void PrintGroup(string[,] group)
 {
-  return random.Next(1, 7);
-}
-
-int GetTargetValue()
-{
-  return random.Next(1, 6);
-}
-
-bool WinOrLose(int target, int roll)
-{
-  return roll > target;
-}
-
-void PlayGame()
-{
-  var play = true;
-
-  while (play)
+  for (int i = 0; i < group.GetLength(0); i++)
   {
-    var target = GetTargetValue();
-    var roll = GetRollValue();
-
-    Console.WriteLine($"Roll a number greater than {target} to win!");
-    Console.WriteLine($"You rolled a {roll}");
-    Console.WriteLine(WinOrLose(target, roll));
-    Console.WriteLine("\nPlay again? (Y/N)");
-
-    play = ShouldPlay();
+    Console.Write($"Group {i + 1}:");
+    for (int j = 0; j < group.GetLength(1); j++)
+    {
+      Console.Write($"{group[i, j]}  ");
+    }
+    Console.WriteLine();
   }
 }
